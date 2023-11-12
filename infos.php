9dde396482e70d2bbd7803aa6dbde9dcdf49c4be
imfabi0 <fabio.puzzo@itiscuneo.eu>
2023-11-12 22:37:54 +0100

    Fix stupid errors

--- infos.php
+++ infos.php
@@ -1,9 +1,9 @@
 <?php
 
     class Infos{
-        private int $emotivity;
-        private int $exp;
-        private int $hunger;
+        private int $emotivity = 0;
+        private int $exp = 0;
+        private int $hunger = 0;
         
 
 
@@ -13,18 +13,17 @@
             $this->$hunger = $hunger;
         }
 
-        public static function __constructor_vuoto(){
-            return __constructor(0,0,0);
-        }
+        public static function __constructor_vuoto(){return __constructor(0,0,0);}
 
-        public function getEmotivity():int{return $emotivity;}
-        public function getExp():int{return $exp;}
-        public function getHunger():int{return $hunger;}
+        public function getEmotivity():int{return $this->emotivity;}
+        public function getExp():int{return $this->exp;}
+        public function getHunger():int{return $this->hunger;}
         
         public function setEmotivity(int $emotivity){ $this->$emotivity = $emotivity;}
         public function setExp(int $exp){ $this->$exp = $exp;}
         public function setHunger(int $hunger){ $this->$hunger = $hunger;}
-    
+        
+        public function toString():string{return "Fame: ".$this->getHunger()."<br>Emotività: ".$this->getEmotivity()."% <br>Esperienza: ".$this->getExp();}
     
     }
 ?>
---
+++ test.php
@@ -0,0 +1,7 @@
+<?php
+    include_once 'infos.php';
+
+    $var = new Infos(1,1,1);
+    echo $var->toString();
+
+?>
