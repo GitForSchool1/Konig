be6b54b44565b7c629de94575e6eba4a787eea27
imfabi0 <fabio.puzzo@itiscuneo.eu>
2023-11-13 12:32:44 +0100

    Add Faces

--- Konig.php
+++ Konig.php
@@ -1,7 +1,21 @@
 <?php 
-/*
-    This is going to be Konig also miss his brain
-*/
+    enum Faces: string{
+        case Normal = '(◕‿‿◕)';
+        case Normal_Right = '( ◕‿◕)';
+        case Normal_Left = '(◕‿◕ )';
+        
+        case Wtf_Normal = '(☉_☉)';
+        case Wtf_Right = '( ☉_☉)';
+        case Wtf_Left = '(☉_☉ )';
+ 
+        case Bored = '(-__-)';
+        case Love = '(♥‿‿♥)';
+        case Sad1 = '(╥☁ ╥ )';
+        case Sad2 = '(ب__ب)';
+        case SuperHappy = '(☼‿‿☼)';
+        case Happy1 = '(ᵔ◡◡ᵔ)';
+        case Happy2 = '(^‿‿^)';
 
+    }
 
 ?>
--- test.php
+++ test.php
@@ -1,6 +1,5 @@
 <?php
     include_once 'infos.php';
     $var = new Infos(1,1,1);
-    echo $var->toString()."<br>";
-
+    echo $var->toString();
 ?>
